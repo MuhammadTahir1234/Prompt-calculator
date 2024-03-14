fd572d05aa3525553ef62f8a09974ef1a3f5f065
tahir.awan1234 <tahir.awan8889@gmail.com>
2024-03-14 09:46:11 +0500

    prompt cal

---
+++ app.js
@@ -0,0 +1,7 @@
+let a = +prompt("enter your first vlaue");
+let b = +prompt("Second value");
+
+document .write("Addition",a+b,"<br>");
+document .write("Subtraction",a-b,"<br>");
+document .write("Multiplication",a*b,"<br>");
+document .write("Divison",a/b,"<br>");
